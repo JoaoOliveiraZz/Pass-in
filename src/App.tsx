a5fb25bead4bc19b53b719d2e550ba238046d5ee
JoaoOliveiraZz <linajoaovictor@gmail.com>
2024-04-02 16:40:36 -0300

    :heavy_plus_sign: Added dependences (Tailwind css and Lucide React)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Smile } from 'lucide-react'
 
 function App() {
-  return <h1>Initial commit!</h1>
+  return (
+    <>
+      <h1>Tailwind and Lucide React Icons</h1>
+      <Smile />
+    </>
+  )
 }
 
 export default App
