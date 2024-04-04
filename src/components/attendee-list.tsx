efa63925d216c44374bad564881714536fefcfcd
JoaoOliveiraZz <linajoaovictor@gmail.com>
2024-04-03 23:25:31 -0300

    :lipstick: Table structure and design

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -3,12 +3,54 @@ import { Search } from 'lucide-react'
 export function AttendeeList(){
 
     return(
-        <div className='flex items-center gap-3'> 
-            <h1 className="font-bold text-2xl">Participantes</h1>
+        <div className='flex flex-col gap-4'>
+            <div className='flex items-center gap-3'> 
+                <h1 className="font-bold text-2xl">Participantes</h1>
 
-            <div className='flex items-center gap-3 w-72 border border-white/10 py-1.5 px-3 rounded-lg'>
-                <Search className='size-4 text-emerald-300' />
-                <input type="text" placeholder="Buscar participante..." className='bg-transparent outline-none' />
+                <div className='flex items-center gap-3 w-72 border border-white/10 py-1.5 px-3 rounded-lg'>
+                    <Search className='size-4 text-emerald-300' />
+                    <input type="text" placeholder="Buscar participante..." className='bg-transparent outline-none' />
+                </div>
+            </div>
+
+            <div className='border border-white/10 rounded-lg'>
+                <table className='w-full'>
+                    <thead>
+                        <tr className='px-4 py-3 border-b border-white/10' >
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'><input type="checkbox" /></th>
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'>Código</th>
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'>Participante</th>
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'>Data da inscrição</th>
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'>Data do check-in</th>
+                            <th className='font-semibold text-white text-left text-sm px-4 py-3'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Array.from({length: 6}).map((_, index) => {
+                                return (
+                                    <tr key={index} className='px-4 py-3 border-b border-white/10'> 
+                                        <td className='px-4 py-3 text-left text-zinc-300'><input type="checkbox" /></td>
+                                        <td className='px-4 py-3 text-left text-zinc-300'>87234</td>
+                                        <td className='px-4 py-3 text-left text-zinc-300 flex flex-col gap-1'>
+                                            <span className='text-white font-semibold text-sm'>João Victor de Lima Oliveira</span>
+                                            <span className='font-normal text-xs'>joaolima.gxp@gmail.com</span>
+                                        </td>
+                                        <td className='px-4 py-3 text-left text-zinc-300'>7 dias atrás</td>
+                                        <td className='px-4 py-3 text-left text-zinc-300'>3 dias atrás</td>
+                                        <td className='px-4 py-3 text-left text-zinc-300'></td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr className='px-4 py-3 text-zinc-300'>
+                            <td className='px-4 py-3 text-sm' colSpan={3}>Mostrando 10 de 228 itens</td>
+                            <td className='px-4 py-3 text-right text-sm' colSpan={3}>Página 1 de 11</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
     )
